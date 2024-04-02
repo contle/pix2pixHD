baea73fb075b4b9383fb960488f45376da623063
Georg Hess <georg.hess@zenseact.com>
2024-04-02 10:55:40 +0200

    follow symlinks

--- pix2pixhd/data/image_folder.py
+++ pix2pixhd/data/image_folder.py
@@ -22,7 +22,7 @@ def make_dataset(dir):
     images = []
     assert os.path.isdir(dir), '%s is not a valid directory' % dir
 
-    for root, _, fnames in sorted(os.walk(dir)):
+    for root, _, fnames in sorted(os.walk(dir,followlinks=True)):
         for fname in fnames:
             if is_image_file(fname):
                 path = os.path.join(root, fname)
